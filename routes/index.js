88684bb15e988e447b471176419d4aaec5629e55
marissacraig <gersen.marissa@gmail.com>
2023-09-18 16:17:13 -0700

    changed api route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const notesRouter = require('./notes');
 
 const app = express();
-// Send all the requests that begin with /notes to the notes router
-app.use('/notes', notesRouter);
+// Send all the requests that begin with api/notes to the notes router
+app.use('api/notes', notesRouter);
 
 module.exports = app;
